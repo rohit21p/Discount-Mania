567ad1cea0a2752d0819101057f1f12513f1e995
Rohit <iamrohit.panjwani@gmail.com>
2019-10-18 15:50:49 +0530

    sends request to api on sign-in and sign-up

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 declare var $: any;
 
@@ -12,8 +13,12 @@ export class NavbarComponent implements OnInit {
   isNewUser: boolean = true;
   isRegUser: boolean = false;
   scrolled = false;
+  name: string;
+  email: string;
+  password: string;
+  paytm: string;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     window.addEventListener('scroll', () => {
       let nav = document.getElementById('nav');
       if ((document.documentElement.scrollTop || document.body.scrollTop) > 61.6) {
@@ -34,6 +39,30 @@ export class NavbarComponent implements OnInit {
   toggleUser() {
     this.isNewUser = !this.isNewUser;
     this.isRegUser = !this.isRegUser;
+  }
+
+  log_in() {
+    if(this.isNewUser) {
+      this.http.post('http://localhost:3000/sign-up', JSON.stringify({
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        paytm: this.paytm
+      })).subscribe((data) => {
+        console.log(data);
+      }, (error) => {
+        console.log(error);
+      });
+    } else {
+      this.http.post('http://localhost:3000/sign-in', JSON.stringify({
+        email: this.email,
+        password: this.password
+      })).subscribe((data) => {
+        console.log(data);
+      }, (error) => {
+        console.log(error);
+      });
+    }
   }
 
 }
