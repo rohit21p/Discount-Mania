39d8d408f3fa25e0a23a8601566aada166dc4648
Rohit <iamrohit.panjwani@gmail.com>
2019-10-27 13:01:57 +0530

    Offers component template ready

--- src/app/offers/offers.component.ts
+++ src/app/offers/offers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OffersComponent implements OnInit {
 
-  constructor() { }
+  offers = [
+    {
+      title: 'Ofsgv svst bst - 1',
+      desc: 'dvnksrnvskrnvgkssgrvrjjjjjjjjj jjjjjjjjjjjjjjjjjsr ggggggggggggggggk mmmmmmmmmmmmv fffffffffffffffn        cdaekmfvjnsfkv fkmnjfvnjdnj',
+    },
+    {
+      title: 'Ofsgv svst bst - 2',
+      desc: 'dvnksrnvskrnvgkssgrvrjjjjjjjjj jjjjjjjjjjjjjjjjjsr ggggggggggggggggk mmmmmmmmmmmmv fffffffffffffffn        cdaekmfvjnsfkv fkmnjfvnjdnj',
+    },
+    {
+      title: 'Ofsgv svst bst - 3',
+      desc: 'dvnksrnvskrnvgkssgrvrjjjjjjjjj jjjjjjjjjjjjjjjjjsr ggggggggggggggggk mmmmmmmmmmmmv fffffffffffffffn        cdaekmfvjnsfkv fkmnjfvnjdnj',
+    },
+    {
+      title: 'Ofsgv svst bst - 4',
+      desc: 'dvnksrnvskrnvgkssgrvrjjjjjjjjj jjjjjjjjjjjjjjjjjsr ggggggggggggggggk mmmmmmmmmmmmv fffffffffffffffn        cdaekmfvjnsfkv fkmnjfvnjdnj',
+    },
+    {
+      title: 'Ofsgv svst bst - 5',
+      desc: 'dvnksrnvskr nvgkssgrvrjjjjjjjj jjjjjjjjjjjjjjjjjjsr ggggggggggggggggk mmmmmmmmmmmmv fffffffffffffffn        cdaekmfvjnsfkv fkmnjfvnjdnj',
+    },
+    {
+      title: 'Ofsgv svst bst - 3',
+      desc: 'dvnksrnvskr nvgkssgrvrjjjjjjjj jjjjjjjjjjjjjjjjjjsr ggggggggggggggggk mmmmmmmmmmmmv fffffffffffffffn        cdaekmfvjnsfkv fkmnjfvnjdnj',
+    },
+    {
+      title: 'Ofsgv svst bst - 4',
+      desc: 'dvnksrnvskr nvgkssgrvrjjjjjjjj jjjjjjjjjjjjjjjjjjsr ggggggggggggggggk mmmmmmmmmmmmv fffffffffffffffn        cdaekmfvjnsfkv fkmnjfvnjdnj',
+    },
+    {
+      title: 'Ofsgv svst bst - 5',
+      desc: 'dvnksrnvskr nvgkssgrvrjjjjjjjj jjjjjjjjjjjjjjjjjjsr ggggggggggggggggk mmmmmmmmmmmmv fffffffffffffffn        cdaekmfvjnsfkv fkmnjfvnjdnj',
+    }
+  ];
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // this.route.snapshot.params['category'];
   }
 
 }
