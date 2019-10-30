61101428168f7fe31408eafdb9b42d298c8c1cfd
Rohit <iamrohit.panjwani@gmail.com>
2019-10-30 15:39:07 +0530

    offer component temporary template ready

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-offer',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferComponent implements OnInit {
 
-  constructor() { }
+  offer = {
+    by: 'Rohit Panjwani',
+    title: 'Placeholder Offer',
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati at facere, dolor dolorem eaque error 
+    natus officia, aliquam molestiae voluptate amet aperiam sint aut consequuntur, laboriosam voluptates aliquid ut vero.`,
+    worth: 200,
+    valid: '20/09/2019',
+    price: 2.65
+  };
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  buy() {
+    this.http.post('http://localhost:3000/setup', JSON.stringify({
+      amount: this.offer.price
+    }), {
+      withCredentials: true
+    }).subscribe(() => {
+      window.open('http://localhost:3000/paytm', '_blank');
+    });
   }
 
 }
