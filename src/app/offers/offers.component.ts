a808d09be5d262c97e3b8f3a724adc8e9c417327
Rohit <iamrohit.panjwani@gmail.com>
2019-10-31 21:15:02 +0530

    added modal to show error if any occurs

--- src/app/offers/offers.component.ts
+++ src/app/offers/offers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+declare var $: any;
+
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
@@ -12,6 +14,8 @@ export class OffersComponent implements OnInit {
   offers = [
   ];
 
+  msg: string;
+
   constructor(private route: ActivatedRoute,
               private http: HttpClient) { }
 
@@ -21,6 +25,9 @@ export class OffersComponent implements OnInit {
     })).subscribe((data: any) => {
       console.log(data);
       this.offers = data.result;
+    }, (error) => {
+      this.msg = 'Can\'t connect to server.';
+      $('#error').modal('show');
     });
   }
 
