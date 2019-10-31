fd03d77cf1105be7e51159cd48e8bc4a311650d4
Rohit <iamrohit.panjwani@gmail.com>
2019-10-31 16:50:04 +0530

    enabled two-way data-binding for Sell form

--- src/app/sell/sell.component.ts
+++ src/app/sell/sell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sell',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellComponent implements OnInit {
 
-  constructor() { }
+  form = {
+    title: '',
+    category: '',
+    predesc: '',
+    comdesc: '',
+    price: '',
+    validity: ''
+  };
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
