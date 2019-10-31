d39d2146c010582dc504b4f79e861ef52145cf06
Rohit <iamrohit.panjwani@gmail.com>
2019-10-31 20:38:46 +0530

    Modal added to show status of creating post

--- src/app/sell/sell.component.ts
+++ src/app/sell/sell.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+declare var $: any;
+
 @Component({
   selector: 'app-sell',
   templateUrl: './sell.component.html',
@@ -17,14 +19,25 @@ export class SellComponent implements OnInit {
     validity: ''
   };
 
+  msg: string;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   create() {
-    this.http.post("http://localhost:3000/create", JSON.stringify(this.form)).subscribe((data) => {
+    this.http.post("http://localhost:3000/create", JSON.stringify(this.form)).subscribe((data: any) => {
       console.log(data);
+      if (data.success) {
+        this.msg = 'A post is succesfuly created.';
+      } else {
+        this.msg = 'Error Occured.';
+      }
+      $('#create-status').modal('show');
+    }, (error) => {
+      this.msg = 'Can\'t connect to server.';
+      $('#create-status').modal('show');
     });
   }
 
