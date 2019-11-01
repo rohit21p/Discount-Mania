6a20c67a623eb6c0d29fa785ce91d7c832a32169
Rohit <iamrohit.panjwani@gmail.com>
2019-11-01 22:47:48 +0530

    a login rest api check is added before transaction occurs

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -25,47 +25,61 @@ export class OfferComponent implements OnInit {
       this.offer = data.result[0];
       if (!data.success) {
         this.msg = 'Some Error Occurred.';
-        this.submsg = `Contact us on dis@mania.com`;
+        this.submsg = `Contact us on dis@mania.com if needed`;
         $('#error-loading').modal('show');
       }
     }, (error) => {
       this.msg = 'Can\'t connect to server.';
-      this.submsg = `Contact us on dis@mania.com`;
+      this.submsg = `Contact us on dis@mania.com if needed`;
       $('#error-loading').modal('show');
     });
   }
 
   buy() {
-    this.http.post('http://localhost:3000/setup', JSON.stringify({
-      amount: this.offer.price,
-      _id : this.offer._id
-    }), {
+    this.http.get('http://localhost:3000/isLoggedIn', {
       withCredentials: true
-    }).subscribe(() => {
-      window.open('http://localhost:3000/paytm', '_blank');
-      this.http.post('http://localhost:3000/payment-status/' + this.offer._id, JSON.stringify({
-      }), {
-        withCredentials: true
-      }).subscribe((data: any) => {
-        console.log(data);
-        if (data.payment) {
-          this.msg = 'Payment Successful.';
-          this.submsg = ' Go to your profile to view offer.';
+    }).subscribe((is: any) => {
+      if (is.LoggedIn) {
+        this.http.post('http://localhost:3000/setup', JSON.stringify({
+          amount: this.offer.price,
+          _id : this.offer._id
+        }), {
+          withCredentials: true
+        }).subscribe(() => {
+          window.open('http://localhost:3000/paytm', '_blank');
+          this.http.post('http://localhost:3000/payment-status/' + this.offer._id, JSON.stringify({
+          }), {
+            withCredentials: true
+          }).subscribe((data: any) => {
+            console.log(data);
+            if (data.payment) {
+              this.msg = 'Payment Successful.';
+              this.submsg = ' Go to your profile to view offer.';
+              $('#error-loading').modal('show');
+            } else {
+              this.msg = 'Payment failed.';
+              this.submsg = `Your payment has failed due to a technical error. Please try again or
+                              use a different method to complete the payment.`;
+              $('#error-loading').modal('show');
+            }
+          }, (error) => {
+            this.msg = 'Can\'t connect to server.';
+            this.submsg = `Contact us on dis@mania.com if needed`;
+            $('#error-loading').modal('show');
+          });
+        }, (error) => {
+          this.msg = 'Can\'t connect to server.';
+          this.submsg = `Contact us on dis@mania.com if needed`;
           $('#error-loading').modal('show');
-        } else {
-          this.msg = 'Payment failed.';
-          this.submsg = `Your payment has failed due to a technical error. Please try again or
-                          use a different method to complete the payment.`;
-          $('#error-loading').modal('show');
-        }
-      }, (error) => {
-        this.msg = 'Can\'t connect to server.';
-        this.submsg = `Contact us on dis@mania.com`;
+        });
+      } else {
+        this.msg = 'Not Logged In';
+        this.submsg = `Log-in first`;
         $('#error-loading').modal('show');
-      });
+      }
     }, (error) => {
       this.msg = 'Can\'t connect to server.';
-      this.submsg = `Contact us on dis@mania.com`;
+      this.submsg = `Contact us on dis@mania.com if needed`;
       $('#error-loading').modal('show');
     });
   }
