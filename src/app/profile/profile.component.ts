b12d3db138a26c449a669ef3cecd08f58d8aa875
Rohit <iamrohit.panjwani@gmail.com>
2019-11-04 00:36:23 +0530

    profile component now using rest api call instead of temporary data

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+declare var $: any;
 
 @Component({
   selector: 'app-profile',
@@ -7,45 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  b_offers = [
-    {
-      _id: 1,
-      title: 'Rohit',
-      predesc: 'dacadc davfav dacqav qecdq ec we cwvswf vswvv'
-    },
-    {
-      _id: 1,
-      title: 'Rohit',
-      predesc: 'dacadc davfav dacqav qecdq ec we cwvswf vswvv'
-    },
-    {
-      _id: 1,
-      title: 'Rohit',
-      predesc: 'dacadc davfav dacqav qecdq ec we cwvswf vswvv'
-    }
-  ];
+  msg: string;
+  profile: any;
+  submsg: string;
 
-  p_offers = [
-    {
-      _id: 1,
-      title: 'Rohit',
-      predesc: 'dacadc davfav dacqav qecdq ec we cwvswf vswvv'
-    },
-    {
-      _id: 1,
-      title: 'Rohit',
-      predesc: 'dacadc davfav dacqav qecdq ec we cwvswf vswvv'
-    },
-    {
-      _id: 1,
-      title: 'Rohit',
-      predesc: 'dacadc davfav dacqav qecdq ec we cwvswf vswvv'
-    }
-  ];
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://localhost:3000/isLoggedIn', {
+      withCredentials: true
+    }).subscribe((is: any) => {
+      if (is.LoggedIn) {
+        this.http.post("http://localhost:3000/profile", JSON.stringify({
+        }), {
+          withCredentials: true
+        }).subscribe((data: any) => {
+          console.log(data);
+          this.profile = data;
+        }, (error) => {
+          this.msg = 'Can\'t connect to server.';
+          this.submsg = 'Contact us at dis@mania.com if needed'
+          $('#profilemodal').modal('show');
+        });
+      } else {
+        this.msg = 'Not Logged In';
+        this.submsg = 'Log-in first';
+        $('#profilemodal').modal('show');
+      }
+    }, (error) => {
+      this.msg = 'Can\'t connect to server.';
+      this.submsg = 'Contact us at dis@mania.com if needed';
+      $('#profilemodal').modal('show');
+    });
+  }
+
+  reqmoney() {
+    this.msg = 'Your Request has been generated.';
+    this.submsg = 'You will recieve money in 2-3 days.'
+    $('#profilemodal').modal('show');
   }
 
 }
